c10a61585cdfd3b205bb92c7d90b152a6369955e
Dani Lyn <dlyn1110@gmail.com>
2018-04-25 15:26:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_second_tier
     :railroads=>
 		{
 			:pieces=>4,
-			:rent_in_dollars=>{}
+			:rent_in_dollars=>{},
 			:names=>{}
 		}
 	}
@@ -42,9 +42,9 @@ def monopoly_with_third_tier
 		},
 		:names=>
 		{
-			:reading_railroad=>{}
-			:pennsylvania_railroad=>{}
-			:b_and_o_railroad=>{}
+			:reading_railroad=>{},
+			:pennsylvania_railroad=>{},
+			:b_and_o_railroad=>{},
 			:shortline=>{}
 		}
 	}
